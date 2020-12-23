fda66c7d669645702938705355eb3407f99ecdb5
Michael Fuget <michaelfuget@gmail.com>
2020-12-23 05:17:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse 
+  end
+  
+  get '/square/:number' do 
+    "#{params[:number].to_i*params[:number].to_i}.to_s"
+  end
+  
   
 end
